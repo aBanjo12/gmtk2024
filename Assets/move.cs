49b3f9906cb9b353dceecc16530a35810d81e172
Landon Kubik <landonlmk@gmail.com>
2024-08-17 13:41:36 -0400

    jump

--- Assets/move.cs
+++ Assets/move.cs
@@ -7,61 +7,25 @@ using UnityEngine.Timeline;
 
 public class move : MonoBehaviour
 {
-    public Transform transform;
     public Rigidbody2D rb;
 
     public bool grounded = false;
-    public float jumpForce = 10f;
-    public float gravity = .1f;
-    public float speedcap = 10f;
-
-    public float speed = 0f;
-    public float acceleration = 0f;
+    public float jumppower = 10;
 
     private List<RaycastHit2D> collisions = new();
     
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     void FixedUpdate()
     {
-        if (!grounded)
+        if (Input.GetKey(KeyCode.Space) && grounded)
         {
-            acceleration -= gravity;
-            speed += acceleration;
-            // if (speed > speedcap)
-            // {
-            //     speed = speedcap;
-            // }
-            speed *= Time.fixedDeltaTime;
-        }
-        else
-        {
-            if (Input.GetKey(KeyCode.Space))
-            {
-                acceleration = jumpForce;
-                grounded = false;
-            }
+            rb.AddForce(new Vector2(0, jumppower), ForceMode2D.Impulse);
+            grounded = false;
         }
         
-        if (rb.Cast(new Vector2(0, -1), collisions, 0.01f) > 0 && acceleration < 0)
+        
+        if (rb.Cast(new Vector2(0, -1), collisions, 0.01f) > 0)
         {
             grounded = true;
-            speed = 0;
-            acceleration = 0;
-        }
-        else
-        {
-            rb.MovePosition(new Vector2(0, speed));
         }
     }
 }
